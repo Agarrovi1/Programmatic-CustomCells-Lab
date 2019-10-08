0738868835b9f62863fc42cd508e2bad7d0c1e5f
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-08 14:29:41 -0400

    initial commit

---
+++ ProgrammaticCustomCell-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticCustomCell-Lab
+//
+//  Created by Angela Garrovillas on 10/8/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
